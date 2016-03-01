e7ee3d2d912d11c6db566f06c809c04a99f53e21
slawko <slawkobond@mail.ru>
2016-03-01 10:36:57 +0200

    Start Iterators

--- ES6/app/src/09_test.js
+++ ES6/app/src/09_test.js
@@ -1,6 +1,23 @@
 console.log('09. Iterators');
 
+let fibonacci = {
+    [Symbol.iterator]() {
+        let pre = 0, cur = 1;
+        return {
+            next() {
+                [pre, cur] = [cur, pre + cur];
+                return { done: false, value: cur }
+            }
+        }
+    }
+};
 
+for (var n of fibonacci) {
+    // truncate the sequence at 1000
+    if (n > 1000)
+        break;
+    console.log(n);
+}
 
 
 console.log('---------------------------');
